1a1208be39bff3e2701d347b9d4b52fce3aa9ec6
Oliver Tacke <o.tacke@posteo.de>
2023-07-06 18:18:32 +0200

    Add constants

--- src/scripts/h5peditor-keyword-extractor.js
+++ src/scripts/h5peditor-keyword-extractor.js
@@ -80,7 +80,9 @@ export default class KeywordExtractor {
 
     // Re-create previously stored keywords
     if (this.params.keywords) {
-      this.buildKeywords(this.params.keywords, 'parseKeywordsComma');
+      this.buildKeywords(
+        this.params.keywords, KeywordExtractor.COMMAND_PARSE_KEYWORDS
+      );
     }
 
     this.$errors = this.$container.find('.h5p-errors');
@@ -127,7 +129,10 @@ export default class KeywordExtractor {
     for (const fieldName in buttonConfig) {
       const command = buttonConfig[fieldName];
 
-      if (!['extractKeywords', 'parseKeywordsComma'].includes(command)) {
+      if (![
+        KeywordExtractor.COMMAND_EXTRACT_KEYWORDS,
+        KeywordExtractor.COMMAND_PARSE_KEYWORDS
+      ].includes(command)) {
         return; // No valid command for button
       }
 
@@ -178,7 +183,7 @@ export default class KeywordExtractor {
   buildKeywords(inputText, command) {
     const keywords = KeywordUtil.getKeywords({
       text: inputText,
-      mode: (command === 'parseKeywordsComma') ?
+      mode: (command === KeywordExtractor.COMMAND_PARSE_KEYWORDS) ?
         'comma-separated' :
         'extract',
       language: H5PEditor.contentLanguage
@@ -187,3 +192,9 @@ export default class KeywordExtractor {
     this.keywordList.addKeywords(keywords);
   }
 }
+
+/** @constant {string} COMMAND_EXTRACT_KEYWORDS Command for keyword extraction, used in semantics.json. */
+KeywordExtractor.COMMAND_EXTRACT_KEYWORDS = 'extractKeywords';
+
+/** @constant {string} COMMAND_PARSE_KEYWORDS Command for keyword separation by comma, used in semantics.json. */
+KeywordExtractor.COMMAND_PARSE_KEYWORDS = 'parseKeywordsComma';
